bb99ba6cb9728afcde92ea6396033ca3b404d79c
dayhaysoos <ndejesus1227@gmail.com>
2019-12-17 23:19:58 -0500

    apollo client added

--- site/t7chicken.com/src/pages/index.js
+++ site/t7chicken.com/src/pages/index.js
@@ -1,21 +1,34 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
+import { useQuery } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import Image from '../components/image'
+import SEO from '../components/seo'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const allCharacters = gql`
+  {
+    hello
+  }
+`
+
+const IndexPage = () => {
+  const { data, error, loading } = useQuery(allCharacters)
+
+  console.log('data', data)
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
