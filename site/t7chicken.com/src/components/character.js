c47eb09689ac3b2c4e861e7e1f8277d6182e8a0c
dayhaysoos <ndejesus1227@gmail.com>
2019-12-21 12:27:41 -0500

    styling changes

--- site/t7chicken.com/src/components/character.js
+++ site/t7chicken.com/src/components/character.js
@@ -5,55 +5,68 @@ import Layout from '../components/layout'
 import Image from '../components/image'
 import SEO from '../components/seo'
 
+const Modal = props => (
+  <div
+    sx={{
+      display: props.ishovering === 'true' ? 'block' : 'none',
+      position: 'fixed',
+      zIndex: 2,
+      left: 0,
+      top: 0,
+      width: '100%',
+      overflow: 'auto',
+      backgroundColor: 'rgba(0,0,0,0.4)',
+    }}
+    {...props}
+  />
+)
+
+const ModalContent = props => (
+  <div
+    sx={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      margin: '15% auto',
+      width: '100%',
+      height: '100%',
+      transition: 'all 1s',
+      animation: 'fade-in 1s',
+    }}
+    {...props}
+  />
+)
+
+const Table = props => (
+  <table
+    sx={{
+      display: 'block',
+    }}
+    {...props}
+  />
+)
+
 const Character = ({ pageContext }) => {
   const [hoverState, setHoverState] = useState({
     isHovering: false,
     preview_url: '',
   })
 
-  console.log(hoverState.isHovering ? 'block' : 'none')
-
   return (
     <Layout>
       <SEO title={`${pageContext.characterData.displayName} frame data`} />
       <h1>{pageContext.characterData.displayName}</h1>
-      <div
-        sx={{
-          display: `${hoverState.isHovering ? 'block' : 'none'}`,
-          position: 'fixed',
-          zIndex: 2,
-          left: 0,
-          top: 0,
-          width: '100%',
-          height: '100%',
-          overflow: 'auto',
-          backgroundColor: 'rgba(0,0,0,0.4)',
-        }}
+      <Modal
+        ishovering={hoverState.isHovering.toString()}
+        onClick={() => setHoverState({ isHovering: false })}
       >
-        <div
-          onClick={() => setHoverState({ isHovering: false })}
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            margin: '15% auto',
-            width: '80%',
-            transition: 'all 1s',
-            animation: 'fade-in 1s',
-          }}
-        >
-          <img src={hoverState.preview_url} />
-        </div>
-      </div>
-      <table
-        sx={{
-          overflowY: 'scroll',
-          height: '400px',
-          display: 'block',
-        }}
-      >
+        <ModalContent>
+          <img sx={{ height: '50%' }} src={hoverState.preview_url} />
+        </ModalContent>
+      </Modal>
+      <Table>
         <caption>{pageContext.characterData.displayName} Frame Data</caption>
-        <thead>
+        <thead sx={{ position: 'sticky', backgroundColor: 'white' }}>
           <tr>
             <th scope="col">Notation</th>
             <th scope="col">Hit Level</th>
@@ -64,21 +77,21 @@ const Character = ({ pageContext }) => {
             <th scope="col">On CH</th>
           </tr>
         </thead>
-        {pageContext.characterData.movelist.map(
-          ({
-            notation,
-            preview_url,
-            move_name,
-            hit_level,
-            damage,
-            speed,
-            on_block,
-            on_hit,
-            on_ch,
-            id,
-          }) => (
-            <tbody key={id}>
-              <tr>
+        <tbody>
+          {pageContext.characterData.movelist.map(
+            ({
+              notation,
+              preview_url,
+              move_name,
+              hit_level,
+              damage,
+              speed,
+              on_block,
+              on_hit,
+              on_ch,
+              id,
+            }) => (
+              <tr key={id}>
                 <th scope="row">
                   <div>{move_name && <p>{move_name}</p>}</div>
                   <p>{notation}</p>
@@ -104,10 +117,10 @@ const Character = ({ pageContext }) => {
                 <td>{on_hit}</td>
                 <td>{on_ch}</td>
               </tr>
-            </tbody>
-          )
-        )}
-      </table>
+            )
+          )}
+        </tbody>
+      </Table>
     </Layout>
   )
 }
