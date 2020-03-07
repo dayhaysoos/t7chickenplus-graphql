e827c851e4cd0676f958957f7eb7dfec2139898c
dayhaysoos <ndejesus1227@gmail.com>
2020-03-06 21:57:09 -0500

    replace react-table with theme-ui

--- site/t7chicken.com/src/components/character.js
+++ site/t7chicken.com/src/components/character.js
@@ -1,58 +1,12 @@
 /**@jsx jsx */
 import React from 'react'
 import { jsx } from 'theme-ui'
-import { useState, useMemo, Fragment } from 'react'
+import { useMemo } from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import Table from '../components/table'
 
-const Modal = props => (
-  <div
-    sx={{
-      display: props.ishovering === 'true' ? 'block' : 'none',
-      position: 'fixed',
-      zIndex: 2,
-      left: 0,
-      top: 0,
-      width: '100%',
-      overflow: 'auto',
-      backgroundColor: 'rgba(0,0,0,0.4)',
-    }}
-    {...props}
-  />
-)
-
-const ModalContent = props => (
-  <div
-    sx={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      margin: '15% auto',
-      width: '100%',
-      height: '100%',
-      transition: 'all 1s',
-      animation: 'fade-in 1s',
-    }}
-    {...props}
-  />
-)
-
-const Span = props => (
-  <span
-    {...props}
-    sx={{
-      display: 'block',
-    }}
-  />
-)
-
 const Character = ({ pageContext }) => {
-  const [hoverState, setHoverState] = useState({
-    isHovering: false,
-    preview_url: '',
-  })
-
   const moveData = useMemo(() => pageContext.characterData.movelist, [])
 
   const columns = React.useMemo(() => [
@@ -61,28 +15,7 @@ const Character = ({ pageContext }) => {
       columns: [
         {
           Header: 'Notation',
-          Cell: ({ cell: { value } }) => (
-            <Fragment>
-              <Span>{value.notation}</Span>
-              <Span>{value.move_name}</Span>
-              {value.preview_url && (
-                <a
-                  href={''}
-                  sx={{ zIndex: 1 }}
-                  onClick={e => {
-                    e.preventDefault()
-                    setHoverState({
-                      isHovering: true,
-                      preview_url: value.preview_url,
-                    })
-                  }}
-                >
-                  View Gif!
-                </a>
-              )}
-            </Fragment>
-          ),
-          accessor: notation => notation,
+          accessor: 'notation',
         },
         {
           Header: 'Hit Level',
@@ -115,15 +48,6 @@ const Character = ({ pageContext }) => {
   return (
     <Layout>
       <SEO title={`${pageContext.characterData.displayName} frame data`} />
-      <h1>{pageContext.characterData.displayName}</h1>
-      <Modal
-        ishovering={hoverState.isHovering.toString()}
-        onClick={() => setHoverState({ isHovering: false })}
-      >
-        <ModalContent>
-          <img sx={{ height: '50%' }} src={hoverState.preview_url} />
-        </ModalContent>
-      </Modal>
       <Table columns={columns} data={moveData} />
     </Layout>
   )
