f124b79656290b7ecf512873d1872c54756aac87
dayhaysoos <ndejesus1227@gmail.com>
2020-04-05 22:31:24 -0400

    user auth logic

--- site/t7chicken.com/src/components/header.js
+++ site/t7chicken.com/src/components/header.js
@@ -1,14 +1,22 @@
 /**@jsx jsx */
 import { jsx, Box, Button, Flex } from 'theme-ui'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import netlifyIdentity from 'netlify-identity-widget'
 
 const Header = ({ siteTitle }) => {
+  const [user, setUser] = useState()
   useEffect(() => {
     netlifyIdentity.init({})
   })
+
+  netlifyIdentity.on('login', user => {
+    netlifyIdentity.close()
+    setUser(user)
+  })
+
+  netlifyIdentity.on('logout', () => setUser())
 
   console.log(netlifyIdentity.currentUser())
   return (
@@ -25,13 +33,31 @@ const Header = ({ siteTitle }) => {
           padding: `1.45rem 1.0875rem`,
         }}
       >
-        <Flex>
-          <Button
-            sx={{ backgroundColor: 'transparent', color: 'white' }}
-            onClick={() => netlifyIdentity.open()}
-          >
-            Log in
-          </Button>
+        <Flex as="nav">
+          <NavLink as={Link} to={'/'} p={2}>
+            Home
+          </NavLink>
+          <NavLink as={Link} to={'/app'} p={2}>
+            Dashboard
+          </NavLink>
+          {!user ? (
+            <Button
+              sx={{ backgroundColor: 'transparent', color: 'white' }}
+              onClick={() => netlifyIdentity.open()}
+            >
+              Log in
+            </Button>
+          ) : (
+            <Flex>
+              <Button
+                sx={{ backgroundColor: 'transparent', color: 'white' }}
+                onClick={() => netlifyIdentity.logout()}
+              >
+                Log out
+              </Button>
+              <p sx={{ p: 2 }}>{user.user_metadata.full_name}</p>
+            </Flex>
+          )}
         </Flex>
       </div>
     </header>
