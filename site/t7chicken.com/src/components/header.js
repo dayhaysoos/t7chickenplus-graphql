11d692eb70902b8672ede84487dcc99f83ccd34f
dayhaysoos <ndejesus1227@gmail.com>
2020-04-05 22:06:16 -0400

    add netlify identity

--- site/t7chicken.com/src/components/header.js
+++ site/t7chicken.com/src/components/header.js
@@ -1,35 +1,42 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+/**@jsx jsx */
+import { jsx, Box, Button, Flex } from 'theme-ui'
+import { useEffect } from 'react'
+import { Link } from 'gatsby'
+import PropTypes from 'prop-types'
+import netlifyIdentity from 'netlify-identity-widget'
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
+const Header = ({ siteTitle }) => {
+  useEffect(() => {
+    netlifyIdentity.init({})
+  })
+
+  console.log(netlifyIdentity.currentUser())
+  return (
+    <header
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        background: `rebeccapurple`,
+        marginBottom: `1.45rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `1.45rem 1.0875rem`,
+        }}
+      >
+        <Flex>
+          <Button
+            sx={{ backgroundColor: 'transparent', color: 'white' }}
+            onClick={() => netlifyIdentity.open()}
+          >
+            Log in
+          </Button>
+        </Flex>
+      </div>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
